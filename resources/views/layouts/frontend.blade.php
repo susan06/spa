8ea8b35e1f04106a8441806cd7bdb2f098b13bf7
Susan Medina <susangmedina@gmail.com>
2017-05-11 12:26:48 -0400

    zoom meta handly

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="HandheldFriendly" content="true" />
     <!--
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
      CSRF Token -->
