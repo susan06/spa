70f2b575d5229748bbfb97e6056e34fdd6f87a97
MEDINA-PC\MEDINA <susangmedina@gmail.com>
2018-04-07 20:32:08 -0500

    quitar el disqus

--- config/app.php
+++ config/app.php
@@ -181,8 +181,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        Laravel\Socialite\SocialiteServiceProvider::class,
-        Roumen\Disqus\DisqusServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class
 
 
     ],
